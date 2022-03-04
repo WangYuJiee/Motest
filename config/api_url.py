ad063bc893caec288fd952c4fdff460f5a90c59e
JoinRi <294182852@qq.com>
2022-03-04 13:46:02 +0800

    config更改
    Signed-off-by: JoinRi <294182852@qq.com>

--- config/api_url.py
+++ config/api_url.py
@@ -1,6 +1,9 @@
 """
 api_url
 """
+# 项目地址
+# BASE_URL = "http://122.229.8.173:3095"
+BASE_URL = "http://192.168.30.58:8080"
 
 sub = '/jfws-pyapi'
 
@@ -8,4 +11,4 @@ sub = '/jfws-pyapi'
 USERS = sub + '/user/users'
 
 # approval
-APPROVAL = sub + '/approval/approval'
+APPROVAL = BASE_URL + sub + '/approval/approval'
