cde04021902c922526d83b8e82153ea69b3fb195
JoinRi <294182852@qq.com>
2022-03-10 09:34:00 +0800

    update
    Signed-off-by: JoinRi <294182852@qq.com>

--- api_test/test_cases/test_api_status.py
+++ api_test/test_cases/test_api_status.py
@@ -6,7 +6,7 @@
 import seldom
 from seldom import file_data, data
 
-from config.api_url import GROPLE_URL
+from config.api_url import GLOBLE_URL
 from utility.requests_interface import BaseRequest
 
 
@@ -27,15 +27,16 @@ class EnableApi(BaseRequest):
     #     ("/course/series", "get", 1, "12")
     # ])
     @file_data(file="excel_api.xlsx", sheet="Sheet1", line=2)
-    def test_excel(self, url, method, data, parameter, desc):
+    def test_status(self, url, method, _data, parameter, desc):
         """
         used file_data test
         """
 
         if parameter:
             url = url+str(parameter)
-        print(GROPLE_URL+url, method, data, parameter, desc)
-        self.request(url=url, method=method)
+        print(GLOBLE_URL+url, method, _data, parameter, desc)
+        self.request(url=url, method=method, data=_data)
+        self.assertStatusCode(200)
 
 
 if __name__ == '__main__':
